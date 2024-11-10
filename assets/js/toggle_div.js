7f1dee908531d197f84026a9e042f59f774cecdc
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2024-11-10 13:08:41 +0700

    6 div chi tiết, 1 brand

--- assets/js/toggle_div.js
+++
@@ -1,8 +0,0 @@
-function toggleDiv(divId) {
-  const divs = document.querySelectorAll(".div_sp_chi_tiet");
-  divs.forEach((div) => (div.style.display = "none"));
-
-  // Hiển thị div có id được nhấn
-  const divToShow = document.getElementById(divId);
-  divToShow.style.display = "block";
-}
