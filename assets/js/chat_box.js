db8db47ad328ce4bde3b4e63a6fa6f17c22c6f74
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2024-11-11 22:55:07 +0700

    chat_box nửa

--- assets/js/chat_box.js
+++ assets/js/chat_box.js
@@ -1,13 +1,16 @@
-// Show chat box when scrolled down 1000px
-window.addEventListener("scroll", function () {
-  const chatButton = document.getElementById("chatButton");
-  if (window.scrollY >= 1000) {
-    chatButton.style.display = "flex";
-  }
-});
-
 // Toggle chat box visibility on button click
 document.getElementById("chatButton").addEventListener("click", function () {
   const chatBox = document.getElementById("chatBox");
-  chatBox.style.display = chatBox.style.display === "block" ? "none" : "block";
+  const hamburgerIcon = document.getElementById("hamburgerIcon");
+  const closeIcon = document.getElementById("closeIcon");
+
+  if (chatBox.style.display === "block") {
+    chatBox.style.display = "none";
+    hamburgerIcon.style.display = "flex";
+    closeIcon.style.display = "none";
+  } else {
+    chatBox.style.display = "block";
+    hamburgerIcon.style.display = "none";
+    closeIcon.style.display = "flex";
+  }
 });
