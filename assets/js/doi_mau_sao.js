e69ee71d3181f1ffface504a1ad6df65aeabdac2
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2024-11-12 12:29:06 +0700

    tính năng rate-start

---
+++ assets/js/doi_mau_sao.js
@@ -0,0 +1,13 @@
+const rateStars = document.querySelectorAll("#rate-product");
+
+rateStars.forEach((rateStar) => {
+  rateStar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const starsList = this.closest(".item").querySelectorAll(".stars i");
+
+    starsList.forEach((star) => {
+      star.classList.toggle("selected");
+    });
+  });
+});
