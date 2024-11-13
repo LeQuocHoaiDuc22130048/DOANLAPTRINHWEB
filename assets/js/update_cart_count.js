2f9b957056efe15879a66d2c055f516411699365
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2024-11-13 16:11:47 +0700

    tăng số lượng đơn hàng qua thẻ span nhỏ

---
+++ assets/js/update_cart_count.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Lấy phần tử hiển thị số lượng giỏ hàng
+  const cartCountElement = document.querySelector(".cart-count");
+
+  // Lấy số lượng giỏ hàng từ localStorage khi trang được tải lại
+  let cartCount = localStorage.getItem("cartCount");
+  if (cartCount) {
+    cartCount = parseInt(cartCount); // Chuyển sang kiểu số
+  } else {
+    cartCount = 0; // Nếu không có giá trị trong localStorage, mặc định là 0
+  }
+
+  // Cập nhật số lượng giỏ hàng khi trang được tải lại
+  updateCartCount(cartCount);
+
+  // Lấy tất cả các nút "Thêm vào giỏ hàng"
+  const btnAddToCart = document.querySelectorAll(".btn_gio_hang");
+
+  // Lặp qua tất cả các nút và gán sự kiện click
+  btnAddToCart.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      cartCount++; // Tăng số lượng giỏ hàng lên 1
+      updateCartCount(cartCount); // Cập nhật số lượng hiển thị trong giỏ hàng
+      localStorage.setItem("cartCount", cartCount); // Lưu số lượng giỏ hàng vào localStorage
+    });
+  });
+
+  // Hàm cập nhật số lượng giỏ hàng trên trang
+  function updateCartCount(count) {
+    cartCountElement.innerText = count; // Cập nhật số lượng giỏ hàng vào phần tử .cart-count
+  }
+});
