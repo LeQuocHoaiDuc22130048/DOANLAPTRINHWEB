374783799d33a5d1b56aa99b2ad29fa1b5f061db
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2024-11-19 23:52:53 +0700

    chỉnh sửa js của phần lọc sản phẩm

--- assets/js/handle-buttom.js
+++ assets/js/handle-buttom.js
@@ -20,4 +20,42 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const dropdownTitles = document.querySelectorAll(".product_regular-title");
+  
+    dropdownTitles.forEach(title => {
+      title.addEventListener("click", () => {
+       
+        const dropdown = title.querySelector(".productfilter-down");
+        const allDropdowns = document.querySelectorAll(".productfilter-down");
+  
+        
+        allDropdowns.forEach(drop => {
+          if (drop !== dropdown) {
+            drop.classList.remove("show");
+            drop.parentElement.querySelector(".product_regular-title").classList.remove("openlist");
+          }
+        });
+  
+        
+        if (dropdown) {
+          dropdown.classList.toggle("show");
+          title.classList.toggle("openlist");
+        }
+      });
+    });
+  
+    
+    document.addEventListener("click", (e) => {
+      if (!e.target.closest(".prodctfilter_filter")) {
+        document.querySelectorAll(".productfilter-down").forEach(dropdown => {
+          dropdown.classList.remove("show");
+          dropdown.parentElement.querySelector(".product_regular-title").classList.remove("openlist");
+        });
+      }
+    });
+  });
+  
   
