2a4ab5de7374b7fe5bdea1cb3cb25b98a01c2afd
hathingan <22130177@st.hcmuaf.edu.vn>
2024-12-21 14:56:03 +0700

    xử lý đăng nhập

--- src/main/java/vn/edu/hcmuaf/fit/eyestyle/DAO/DB/JDBIConnect.java
+++ src/main/java/vn/edu/hcmuaf/fit/eyestyle/DAO/DB/JDBIConnect.java
@@ -2,10 +2,12 @@ package vn.edu.hcmuaf.fit.eyestyle.DAO.DB;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
 import vn.edu.hcmuaf.fit.eyestyle.DAO.Model.BrandsModel;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class JDBIConnect {
     static String url = "jdbc:mysql://" + DBProperties.host() + ":" + DBProperties.port() + "/" + DBProperties.dbname() + "?" + DBProperties.option();
@@ -35,5 +37,18 @@ public class JDBIConnect {
 
     public static void main(String[] args) {
 
+        Jdbi jdbi = get();
+
+        jdbi.useHandle(handle -> {
+            List<Map<String, Object>> results = handle.createQuery("SELECT * FROM categories")
+                    .mapToMap()
+                    .list();
+
+            for (Map<String, Object> row : results) {
+                System.out.println(row);
+            }
+        });
+        }
+
     }
-}
+
