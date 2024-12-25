dc3f3b187c783d6eca04ac43094cbef5d5ea7603
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2024-12-25 23:25:02 +0700

    Thêm các model

---
+++ src/main/java/vn/edu/hcmuaf/fit/doanweb/DAO/Model/Categories.java
@@ -0,0 +1,14 @@
+package vn.edu.hcmuaf.fit.doanweb.DAO.Model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class Category {
+    private int id;
+    private String name;
+    private int status;
+    private int hot;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
