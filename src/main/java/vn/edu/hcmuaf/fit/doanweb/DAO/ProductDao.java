6626cd117805810fcebcf75c3dc796c912a8a86b
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2025-01-15 09:51:07 +0700

    them model interface ProductDao

---
+++ src/main/java/vn/edu/hcmuaf/fit/doanweb/DAO/ProductDao.java
@@ -0,0 +1,18 @@
+package vn.edu.hcmuaf.fit.doanweb.DAO;
+
+import vn.edu.hcmuaf.fit.doanweb.DAO.Model.Discounts;
+import vn.edu.hcmuaf.fit.doanweb.DAO.Model.Product;
+import vn.edu.hcmuaf.fit.doanweb.DAO.Model.ProductImage;
+
+import java.util.List;
+
+public interface ProductDao {
+
+    public Product getProductById(int id);
+
+    List<Product> getProductsByGender(int gender);
+
+    List<ProductImage> getImagesByProductId(int productId);
+
+    public Discounts getActiveDiscounts();
+}
